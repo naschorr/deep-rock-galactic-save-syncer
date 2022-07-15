588b6da5b3d4461231970a711d727ba9afdd2b45
Nick Schorr <naschorr@gmail.com>
2022-07-15 01:24:38 -0700

    Improved SemanticVersioning model
    - Added support for comparisons
    - Added support for a prerelease extension at the end

--- Core/Models/SemanticVersion.cs
+++ Core/Models/SemanticVersion.cs
@@ -1,34 +1,143 @@
-﻿namespace Core.Models
+﻿using System.Text.RegularExpressions;
+
+namespace Core.Models
 {
-    public class SemanticVersion
+    public class SemanticVersion : IComparable<SemanticVersion>
     {
+        private Regex _SemanticVersionRegex = new Regex(@"\D*(\d+)\.(\d+)\.(\d+)-?(.*)");
+
         public readonly int Major;
         public readonly int Minor;
         public readonly int Patch;
+        public readonly string? Extension;
 
         // Constructors
 
-        public SemanticVersion(int major, int minor, int patch)
+        public SemanticVersion(int major, int minor, int patch, string? extension)
         {
             Major = major;
             Minor = minor;
             Patch = patch;
+            Extension = extension;
         }
 
-        public SemanticVersion(IEnumerable<int> version) : this(version.ElementAt(0), version.ElementAt(1), version.ElementAt(2)) { }
+        public SemanticVersion(string version)
+        {
+            // This isn't super robust, however since I control the semver tags, this is fine.
+            var matches = _SemanticVersionRegex.Match(version);
 
-        public SemanticVersion(string version) : this(SemanticVersion.ConvertVersionStringToArray(version)) { }
+            if (matches.Success)
+            {
+                Major = int.Parse(matches.Groups[1].Value);
+                Minor = int.Parse(matches.Groups[2].Value);
+                Patch = int.Parse(matches.Groups[3].Value);
+                Extension = matches.Groups[4].Value;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid version string provided, can't extract semantic versiond data from it.");
+            }    
+        }
 
         // Methods
 
-        private static int[] ConvertVersionStringToArray(string version)
-        {
-            return version.Split('.').Select(e => int.Parse(e)).ToArray();
-        }
-
         public override string ToString()
         {
-            return $"{Major}.{Minor}.{Patch}";
+            return $"{Major}.{Minor}.{Patch}{((Extension != null && Extension.Length > 0) ? $"-{Extension}" : "")}";
+        }
+
+        // Comparison
+
+        public int CompareTo(SemanticVersion? other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return 1;
+            }
+
+            if (Major > other.Major)
+            {
+                return 1;
+            }
+            else if (Major < other.Major)
+            {
+                return -1;
+            }
+
+            if (Minor > other.Minor)
+            {
+                return 1;
+            }
+            else if (Minor < other.Minor)
+            {
+                return -1;
+            }
+
+            if (Patch > other.Patch)
+            {
+                return 1;
+            }
+            else if (Patch < other.Patch)
+            {
+                return -1;
+            }
+
+            bool extensionIsFalsy = Extension == null || Extension.Length == 0;
+            bool otherExtensionIsFalsy = other.Extension == null || other.Extension.Length == 0;
+            if (extensionIsFalsy && !otherExtensionIsFalsy)
+            {
+                return 1;
+            }
+            else if (extensionIsFalsy && otherExtensionIsFalsy || !extensionIsFalsy && !otherExtensionIsFalsy)
+            {
+                return 0;
+            }
+            else if (!extensionIsFalsy && otherExtensionIsFalsy)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+
+        public static bool operator ==(SemanticVersion? one, SemanticVersion? two)
+        {
+            if (ReferenceEquals(one, null))
+            {
+                return ReferenceEquals(two, null);
+            }
+
+            return one.CompareTo(two) == 0;
+        }
+
+        public static bool operator !=(SemanticVersion? one, SemanticVersion? two)
+        {
+            if (ReferenceEquals(one, null))
+            {
+                return !ReferenceEquals(two, null);
+            }
+
+            return one.CompareTo(two) != 0;
+        }
+
+        public static bool operator >(SemanticVersion one, SemanticVersion two)
+        {
+            return one.CompareTo(two) > 0;
+        }
+
+        public static bool operator <(SemanticVersion one, SemanticVersion two)
+        {
+            return one.CompareTo(two) < 0;
+        }
+
+        public static bool operator >=(SemanticVersion one, SemanticVersion two)
+        {
+            return one.CompareTo(two) >= 0;
+        }
+
+        public static bool operator <=(SemanticVersion one, SemanticVersion two)
+        {
+            return one.CompareTo(two) <= 0;
         }
     }
 }
