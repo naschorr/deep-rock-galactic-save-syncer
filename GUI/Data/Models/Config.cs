699f0fb6b64bda56af2a8e47660e0a91adf31e8e
Nick Schorr <naschorr@gmail.com>
2023-01-12 22:36:06 -0800

    Updated `ConfigLoaderService` to generate a `Config` model
    - Updated config loader references to read from the new config object

---
+++ GUI/Data/Models/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace GUI.Data.Models
+{
+    public class Config
+    {
+        public string? RepoUrl { get; set; }
+        public string? WikiUrl { get; set; }
+        public string? UpdateCheckUrl { get; set; }
+        public string? ExitSteamExampleUrl { get; set; }
+        public int? OverwriteFileRefreshIgnoreLockChangesMilliseconds { get; set; }
+    }
+}
