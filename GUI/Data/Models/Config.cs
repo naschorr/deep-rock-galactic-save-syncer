ec6926ff3bb8942393bfd95672ac648173d371cd
Nick Schorr <naschorr@gmail.com>
2023-01-12 23:21:02 -0800

    Tweaked Config class to support read only members

--- GUI/Data/Models/Config.cs
+++ GUI/Data/Models/Config.cs
@@ -1,11 +1,33 @@
-﻿namespace GUI.Data.Models
+﻿using System.Text.Json.Serialization;
+
+namespace GUI.Data.Models
 {
     public class Config
     {
-        public string? RepoUrl { get; set; }
-        public string? WikiUrl { get; set; }
-        public string? UpdateCheckUrl { get; set; }
-        public string? ExitSteamExampleUrl { get; set; }
-        public int? OverwriteFileRefreshIgnoreLockChangesMilliseconds { get; set; }
+        public string? RepoUrl { get; }
+        public string? WikiUrl { get; }
+        public string? UpdateCheckUrl { get; }
+        public string? ExitSteamExampleUrl { get; }
+        public int? OverwriteFileRefreshIgnoreLockChangesMilliseconds { get; }
+
+        // Give the deserializer a constructor to work with, otherwise it'll null all the read only values
+        [JsonConstructor]
+        public Config(
+            string? repoUrl,
+            string? wikiUrl,
+            string? updateCheckUrl,
+            string? exitSteamExampleUrl,
+            int? overwriteFileRefreshIgnoreLockChangesMilliseconds
+        )
+        {
+            RepoUrl = repoUrl;
+            WikiUrl = wikiUrl;
+            UpdateCheckUrl = updateCheckUrl;
+            ExitSteamExampleUrl = exitSteamExampleUrl;
+            OverwriteFileRefreshIgnoreLockChangesMilliseconds = overwriteFileRefreshIgnoreLockChangesMilliseconds;
+        }
+
+        // Fallback null member constructor
+        public Config() { }
     }
 }
